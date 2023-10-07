e9ec3a4b8067fe6c8c4732d2d9ced28163d7b1fe
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-10-07 10:15:07 +0300

    add

--- todo-list/client/src/App.js
+++ todo-list/client/src/App.js
@@ -3,58 +3,77 @@ import Footer from './components/Footer'
 import Headers from './components/Header';
 import Loading from './components/Loading';
 import TableRolls from './components/TableRolls';
-import{useEffect,useState} from "react"
+import { useEffect, useState } from "react"
 function App() {
-const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([])
+const [isLoading,setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(`http://localhost:3030/jsonstore/todos`)
+            .then(res => res.json())
+            .then(data => {
+                const result = Object.keys(data).map(id => ({ id, ...data[id] }))
+
+                setTodos(result);
+
+                setIsLoading(false)
+
+            })
 
 
-useEffect(() => {
-  fetch(`http://localhost:3030/jsonstore/todos`)
-  .then(res=> res.json())
-  .then(data=> {
-const result = Object.keys(data).map(id =>({id, ...data[id]}))
+    }, []);
 
-setTodos(result);
+    const todoStatus = (id) => {
 
-console.log(result)
+        setTodos(oldState => oldState.map(todos => todos.id === id ? ({ ...todos, isCompleted: !todos.isCompleted }) : todos))
+    }
 
-  })
+    const onAdd = () => {
+        const lastId = todos[todos.length - 1].id
+        const text = prompt('Add new Task')
 
-
-},[])
-  return (
-
-    <div>
-    <div className="App">
-
-      <Headers />
+        const newState = {id: lastId + 1, text, isCompleted:false}
+        setTodos(oldState => [newState,...oldState, ])
+    }
 
 
-  <main className="main">
+    return (
+
+        <div>
+            <div className="App">
+
+                <Headers />
 
 
-    <section className="todo-list-container">
-      <h1>Todo List</h1>
+                <main className="main">
 
-      <div className="add-btn-container">
-        <button className="btn">+ Add new Todo</button>
-      </div>
 
-      <div className="table-wrapper">
+                    <section className="todo-list-container">
+                        <h1>Todo List</h1>
 
- 
-       {/* <Loading/> */}
+                        <div className="add-btn-container">
+                            <button className="btn" onClick={onAdd}>+ Add new Todo</button>
+                        </div>
 
- <TableRolls todos={todos}/>
-      </div>
-    </section>
-  </main>
+                        <div className="table-wrapper">
 
-<Footer />
 
-    </div>
-    </div>
-  );
+                       
+                            {isLoading ?
+                             <Loading/> 
+                            :
+                             <TableRolls todos={todos} todoStatus={todoStatus} />}
+
+                           
+                        </div>
+                    </section>
+                </main>
+
+                <Footer />
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- todo-list/client/src/components/TableRolls.js
+++ todo-list/client/src/components/TableRolls.js
@@ -1,6 +1,6 @@
 export default function TableRolls({
     todos,
-
+    todoStatus
 
 }) {
 
@@ -18,11 +18,11 @@ export default function TableRolls({
 
                 {todos.map(todo => (
 
-                    <tr key={todo.id} className={`todo ${todo.isCompleted && "is-completed"}`}>
+                    <tr key={todo.id} className={`${todo.isCompleted ? "is-completed": "todo"}`}>
                         <td>{todo.text}</td>
                         <td>{todo.isCompleted? 'Complete' : "Not Complete" }</td>
                         <td className="todo-action">
-                            <button className="btn todo-btn" onClick={onChange(todo.id)}>Change status</button>
+                            <button className="btn todo-btn" onClick={()=> todoStatus(todo.id)}>Change status</button>
                         </td>
                     </tr>
 
